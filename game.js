0644cf46e89e1cf6963abed681c0b4461789d252
Elwin-CR <145423474+Elwin-CR@users.noreply.github.com>
2025-04-08 09:06:39 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,153 @@
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+const pamanFrames = document.getElementById("animation");
+const ghostFrames = document.getElementById("ghosts"); 
+
+let createRect = (x, y, width, height, color) =>
+{
+    canvasContext.fillStyle = color;
+    canvasContext.fllRect(x, y, width, height,);
+
+}
+
+const Direction_right = 4;
+const Direction_left = 2;
+const Direction_down = 1;
+const Direction_up = 3;
+
+let lives = 3;
+let ghostCount = 4;
+let ghostImageLocations = [
+    { x: 0, y: 0 },
+    { x: 176, y: 0 },
+    { x: 0, y: 121 },
+    { x: 176, y: 121 },
+];
+
+let fps = 30;
+let pacman;
+let oneBlockSize = 20;
+let score = 0;
+let ghosts = [];
+let wallspaceWidth = oneBlockSize/1.6;
+let wallOffset = (oneBlockSize - wallspaceWidth);
+let wallInnerColor = "black";
+
+let map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
+    [1, 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1],
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+let randomTargetsForGhosts = [
+    { x:1 * oneBlockSize, y: 1 * oneBlockSize },
+    { x:1 * oneBlockSize, y: (map.length - 2) * oneBlockSize },
+    { x:(map[0].length) - 2 * oneBlockSize, y: oneBlockSize },
+    {
+        x: (map[0].length - 2) * oneBlockSize,
+        y: (map.length - 2) * oneBlockSize,
+    },
+];
+    
+let createNewPacman = () => 
+{
+    pacman = new Pacman(
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize/5
+    );
+};
+
+let gameLoop = () => 
+{
+    update();
+    draw();
+}
+
+let gameInterval = setInterval(gameLoop, 1000/fps);
+
+let restartPacmanandGhosts = () =>
+{
+    createNewPacman();
+    createGhosts();
+}
+
+let onGhostCollision = () =>
+{
+    lives--;
+    restartPacmanandGhosts();
+    if(lives == 0){
+    }
+}
+
+let update = () =>
+{
+    pacman.moveProcess();
+    pacman.eat();
+    updateGhosts();
+    if(pacman.checkGhostCollsion(ghosts)){
+        onGhostCollision();
+
+    }
+};
+
+let drawFood = () =>
+{
+    for(let i = 0 ; i < map.length ; i++){
+        for(let j = 0 ; j < maop[0].length ; j++) {
+            if(map[i][j] == 2){
+                createRect{
+                    createRect(
+                        j * oneBlockSize + oneBlockSize / 3,
+                        i * oneBlockSize + oneBlockSize / 3,
+                        oneBlockSize / 3,
+                        oneBlockSize / 3,
+                        "#FEB897"
+                    );
+                }
+            }
+        }
+    };
+}   
+let drawRemainigLives = () =>    
+{
+    canvas.Context.Font = "20px Emulogic";
+    canvas.Context.fillStyle = "white";
+    canvas.Context.filltext("Lives: ", 220, oneBlockSize * (map.length) + 1);
+
+    for(let i = 0 ; i < lives ; i++){
+        canvasContext.drawImage(
+            pacmanFrames,
+            2 * oneBlockSize,
+            0,
+            oneBlockSize,
+            oneBlockSize,
+            350 + i * oneBlockSize,
+            oneBlockSize * map.length + 2,
+            oneBlockSize,
+            oneBlockSize
+        );
+    }
+};
